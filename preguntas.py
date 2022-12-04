2286646ee9d831f76cd9f256f3614f9aeaa1d46e
Wilmer Renteria cespedes <wilmer@MacBook-Pro-de-Wilmer.local>
2022-12-04 06:41:52 -0500

    update

--- preguntas.py
+++ preguntas.py
@@ -183,7 +183,7 @@ def pregunta_02():
     (X_train, X_test, y_train, y_test,) = train_test_split(
         X,
         y,
-        test_size=100,
+        test_size=0.1,
         random_state=123,
     )
 
@@ -200,32 +200,29 @@ def pregunta_03():
     # Importe SVC
     # Importe OneHotEncoder
     # Importe Pipeline
-    from ____ import ____
+    from sklearn.compose import make_column_transformer, make_column_selector
+    from sklearn.svm import SVC
+    from sklearn.preprocessing import OneHotEncoder
+    from sklearn.pipeline import Pipeline
 
     # Cargue las variables.
-    X_train, _, y_train, _ = pregunta_02()
+    x_train, x_test, y_train, y_test = pregunta_02()
 
     # Cree un objeto ColumnTransformer que aplique OneHotEncoder a las columnas
     # tipo texto. Use make_column_selector para seleccionar las columnas. Las
     # columnas numéricas no deben ser transformadas.
     columnTransformer = make_column_transformer(
-        (
-            ____(),
-            ____(____=____),
-        ),
-        remainder=____,
+        (OneHotEncoder(), make_column_selector(dtype_include=object)),
+        remainder="passthrough",
     )
 
     # Cree un pipeline que contenga el columnTransformer y el modelo SVC.
-    pipeline = ____(
-        steps=[
-            ("____", ____),
-            ("____", ____),
-        ],
+    pipeline = Pipeline(
+        steps=[("columntransformer", columnTransformer), ("svc", SVC())],
     )
 
     # Entrene el pipeline con los datos de entrenamiento.
-    ____.____(____, ____)
+    pipeline.fit(x_train, y_train)
 
     # # Retorne el pipeline entrenado
     return pipeline
@@ -237,7 +234,7 @@ def pregunta_04():
     """
 
     # Importe confusion_matrix
-    from ____ import ____
+    from sklearn.metrics import confusion_matrix
 
     # Obtenga el pipeline de la pregunta 3.
     pipeline = pregunta_03()
@@ -246,14 +243,14 @@ def pregunta_04():
     X_train, X_test, y_train, y_test = pregunta_02()
 
     # Evalúe el pipeline con los datos de entrenamiento usando la matriz de confusion.
-    cfm_train = ____(
-        y_true=____,
-        y_pred=____.____(____),
+    cfm_train = confusion_matrix(
+        y_true=y_train,
+        y_pred=pipeline.predict(X_train),
     )
 
-    cfm_test = ____(
-        y_true=____,
-        y_pred=____.____(____),
+    cfm_test = confusion_matrix(
+        y_true=y_test,
+        y_pred=pipeline.predict(X_test),
     )
 
     # Retorne la matriz de confusion de entrenamiento y prueba
